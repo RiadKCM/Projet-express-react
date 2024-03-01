05a686869646321dce4abed27bb9bad4e09ba53d
Riad Kacem <riad@MacBook-Air-de-Riad.local>
2024-03-01 12:06:35 +0100

    without test sql

--- backend/server.js
+++ backend/server.js
@@ -14,39 +14,6 @@ const db = mysql.createConnection({
     port: 8889
 })
 
-const createUsersTableQuery = `
-    CREATE TABLE IF NOT EXISTS users (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      username VARCHAR(255) NOT NULL,
-      email VARCHAR(255) NOT NULL,
-      password VARCHAR(255) NOT NULL
-    )
-  `;
-
-const insertTest = `
-INSERT INTO users (username, email, password) VALUES ('riri', 'riadcdd@gmail.com', 'Ridock');
-
-    `
-
-db.connect((err) => {
-    if(err){
-        console.log("ntm")
-        return;
-    }
-    db.query('CREATE DATABASE IF NOT EXISTS crud',(err,result) => {
-        if(err){
-            console.log("flop")
-        }
-
-    });
-    db.query(createUsersTableQuery,(err,result) => {
-        if(err){
-            console.log("flop de la table")
-        }
-    })
-    db.query(insertTest)
-});
-
 app.use(cors())
 
 app.get('/', (req , res) => {
